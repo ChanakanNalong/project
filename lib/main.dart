91c746af09e3f2f261dcf7dfb687842484262116
ChanakanNalong <chanakan111138526@gmail.com>
2024-10-13 16:47:32 +0700

    Deleted model,app_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app_screen/login.dart';
+import 'app_screen/menu_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +8,12 @@ void main() {
 // ส่วนของ Stateless widget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Smart Spots Parking',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      title: 'First Flutter App',
       // home: FirstScreen(),
       initialRoute: '/menu', // สามารถใช้ home แทนได้
       routes: {
